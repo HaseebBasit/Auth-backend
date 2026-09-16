00d3081275f00fb1df73eff571a8e042d262c42c
HaseebBasit <haseebbasit2707@gmail.com>
2026-09-16 21:54:45 +0500

    working

--- src/db/db.js
+++ src/db/db.js
@@ -1,23 +1,26 @@
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const { Pool } = pg;
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    port: 5432,
-    database : "postgres",
-    password : "admin"
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
 });
 
 pool
-    .connect()
-    .then((res) => {
-        console.log('Postgres DB connected successfully!');
-        res.release();
-    })
-    .catch((err) => {
-        console.log('Err while connection with postgres');
-    });
+  .connect()
+  .then((res) => {
+    console.log("Postgres DB connected successfully!");
+    res.release();
+  })
+  .catch((err) => {
+    console.log("Err while connection with postgres:", err.message);
+  });
 
 export default pool;
