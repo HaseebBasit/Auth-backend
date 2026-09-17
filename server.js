8cd79903d48732bdca5d9043c3526064865c1a4c
HaseebBasit <haseebbasit2707@gmail.com>
2026-09-18 00:22:01 +0500

    fixed

--- server.js
+++ server.js
@@ -31,18 +31,14 @@ app.use(express.json());
 // ======================================================
 
 const transporter = nodemailer.createTransport({
-
     host: process.env.SMTP_HOST,
-
     port: Number(process.env.SMTP_PORT),
-
-    secure: false,
-
+    secure: Number(process.env.SMTP_PORT) === 465,
+    family: 4,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD
     }
-
 });
 
 
