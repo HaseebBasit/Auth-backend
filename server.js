7841e4853ac3a96af8c7b9b97ede7b8ea52ffe70
HaseebBasit <haseebbasit2707@gmail.com>
2026-09-18 00:00:07 +0500

    checking

--- server.js
+++ server.js
@@ -10,8 +10,20 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://auth-frontend-three-omega.vercel.app/',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+
 app.use(express.json());
+
 
 
 // ======================================================
