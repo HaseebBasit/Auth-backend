5450e99cb5bd6f801f24a1fc51513b2468e7d463
HaseebBasit <haseebbasit2707@gmail.com>
2026-09-17 23:39:37 +0500

    updated port

--- server.js
+++ server.js
@@ -1014,12 +1014,8 @@ app.post("/password/reset", async (req, res) => {
 // ==================== START SERVER ====================
 // ======================================================
 
-app.listen(5050, () => {
+const PORT = process.env.PORT || 5050;
 
-    console.log(
-
-        "Server is running on port 5050"
-
-    );
-
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
